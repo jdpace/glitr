befd2bfcd5525d04716bf194aa86138b38c8ea6b
Chad Humphries <chad.humphries@gmail.com>
2011-08-09 10:07:28 -0400

    Resolving failing tests by resetting cache_store

--- spec/glitr/config_spec.rb
+++ spec/glitr/config_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe Glitr::Config do
 
   context "#configure" do
+    around do |ex|
+      original_cache_store = Glitr.config.cache_store
+      ex.run
+      Glitr.config.cache_store = original_cache_store
+    end
+
     it "saves config values" do
       Glitr.configure do |config|
         config.cache_store = 'Foo'
